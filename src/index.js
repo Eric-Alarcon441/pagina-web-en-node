a2ece3e9eb4e4174c82cf26f4ac01ceee1e384f2
Eric-Alarcon441 <ericalarcon441@gmail.com>
2021-10-06 18:48:09 -0500

    Proyecto finalizado

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+const { join } = require('path');
+
+//configuraciones
+app.set('port', 3000 || process.ENV.PORT);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(morgan('dev'));
+//rutas
+app.use(require('./routes/index'));
+//archivos staticos
+app.use(express.static(path.join(__dirname, 'public')));
+//escuchando el servidor
+app.listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'));
+});
